c52230de4ee153a5f5aefc42917fbe9ef5e80b06
Sam <samlima028@gmail.com>
2025-05-01 23:16:39 -0400

    Add files via upload

---
+++ my-first-node-project/app.js
@@ -0,0 +1,8 @@
+// Using the built-in 'path' module
+const path = require('path');
+
+// Join two paths together
+const filePath = path.join(__dirname, 'test.txt');
+
+// Print to console
+console.log('Generated file path:', filePath);
